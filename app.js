7aa4e195c3525cddb191d18f3d1336605342fd23
luoyongjie <luoyongjie@bigo.sg>
2021-06-23 22:16:38 +0800

    :feat init

--- app.js
+++ app.js
@@ -13,9 +13,38 @@ const server = https.createServer(credentials, app)
 
 const wss = new WebSocket.Server({ server });
 
+function spanPromise({py, hz, order}) {
+  return new Promise((resolve,reject)=>{
+    const pythonProcess = child_process.spawn('python', [`/home/pi/work/stepper-motor/stepper-${py}.py`, hz, order]);
+    resolve(pythonProcess)
+  })
+}
+
+let carProcess = null;
+let t = null;
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
     data = JSON.parse(data);
+
+    if(!carProcess){
+      const definedQuery = {hz:6400, order:"forward"};
+      const {hz, order, sec} = {...definedQuery, ...data};
+      carProcess = await spanPromise({py:"soft", hz, order});
+      t = setTimeout(async ()=>{
+        carProcess.kill();
+        carProcess=null;
+        t=null;
+        await spanPromise({py:"stop"})
+      }, 1000)
+    }else if(t!==null){
+      t = setTimeout(async ()=>{
+        carProcess.kill();
+        carProcess=null;
+        t=null;
+        await spanPromise({py:"stop"})
+      }, 1000)
+    }
+
     console.log('received: %s', data);
   });
 
