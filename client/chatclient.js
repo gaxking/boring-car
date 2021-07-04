5d5294b6d6996e6d662b9c8c278f4ce7bb080b94
luoyongjie <luoyongjie@bigo.sg>
2021-07-04 10:25:35 +0800

    :feat init

--- client/chatclient.js
+++ client/chatclient.js
@@ -38,7 +38,7 @@ var clientID = 0;
 //
 
 var mediaConstraints = {
-  audio: true,            // We want an audio track
+  //audio: true,            // We want an audio track
   video: {
     aspectRatio: {
       ideal: 1.333333     // 3:2 aspect is preferred
