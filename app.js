63dcd9655c3404ff74e3d088e1e6474264bd5084
luoyongjie <luoyongjie@bigo.sg>
2021-07-04 11:36:52 +0800

    :feat init

--- app.js
+++ app.js
@@ -122,7 +122,9 @@ wss.on('connection', function connection(ws) {
       //await stop();
     }
 
-    ultrasound  && ultrasound(dir);
+    setTimeout(()=>{
+      ultrasound  && ultrasound(dir);
+    }, 10)
   }
 
   ultrasound('left');
