d92528e9eb94c7a78ed46f30e0509053691a0017
luoyongjie <luoyongjie@bigo.sg>
2021-07-12 22:16:10 +0800

    :feat init

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ function mgPromise({deg}) {
 
 function ultrasoundPromise(){
   return new Promise((resolve,reject)=>{
+    resolve({left:10, right:10})
+    return;
     const pythonProcess = child_process.spawn('python', [`/home/pi/work/ultrasound/index.py`]);
 
    let res;
